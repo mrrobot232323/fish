1e2ce0c803f6ac1ae5664bd82774f13b54124117
MrRobot <at16690624@gmail.com>
2025-11-30 05:26:22 +0530

    ste

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+});
 
